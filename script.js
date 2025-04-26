7c54776c184765eb42315113842c0cecc491348b
Johnson Thomas <johnyquest7@outlook.com>
2025-04-25 23:16:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ async function loadModel() {
 
     try {
         generator = await pipeline("text-generation", modelName, {
-            dtype: "int8", device: "webgpu", progress_callback: handleProgress,
+            dtype: "q4", device: "webgpu", progress_callback: handleProgress,
         });
 
         if (!generator || !generator.tokenizer) {
